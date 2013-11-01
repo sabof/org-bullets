22aaffab6309df0e3291670ed42ef569e753d0e6
sabof <esabof@gmail.com>
2013-11-01 01:35:37 +0000

    Added inline task support

--- org-bullets.el
+++ org-bullets.el
@@ -82,12 +82,20 @@ Should this be undesirable, one can remove them with
   nil nil nil
   (let* (( keyword
            `(("^\\*+ "
-              (0 (let (( level (- (match-end 0) (match-beginning 0) 1)))
+              (0 (let* (( level (- (match-end 0) (match-beginning 0) 1))
+                        ( is-inline-task
+                          (and (boundp 'org-inlinetask-min-level)
+                               (>= level org-inlinetask-min-level))))
                    (compose-region (- (match-end 0) 2)
                                    (- (match-end 0) 1)
                                    (org-bullets-level-char level))
+                   (when is-inline-task
+                     (compose-region (- (match-end 0) 3)
+                                     (- (match-end 0) 2)
+                                     (org-bullets-level-char level)))
                    (when (facep org-bullets-face-name)
-                     (put-text-property (- (match-end 0) 2)
+                     (put-text-property (- (match-end 0)
+                                           (if is-inline-task 3 2))
                                         (- (match-end 0) 1)
                                         'face
                                         org-bullets-face-name))
